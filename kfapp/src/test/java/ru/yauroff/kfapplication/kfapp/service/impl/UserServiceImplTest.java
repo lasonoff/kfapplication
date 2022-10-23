ca6310666e05cdd75cd3cda10ced5cf908948b1a
lasonoff <lason-vrn@yandex.ru>
2022-10-23 19:04:06 +0300

    Add test user service

---
+++ kfapp/src/test/java/ru/yauroff/kfapplication/kfapp/service/impl/UserServiceImplTest.java
@@ -0,0 +1,45 @@
+package ru.yauroff.kfapplication.kfapp.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.yauroff.kfapplication.kfapp.model.User;
+import ru.yauroff.kfapplication.kfapp.repository.RoleRepository;
+import ru.yauroff.kfapplication.kfapp.repository.UserRepository;
+import ru.yauroff.kfapplication.kfapp.service.KafkaService;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+    @Mock
+    private UserRepository mockUserRepository;
+
+    @Mock
+    private RoleRepository mockRoleRepository;
+
+    @Mock
+    private KafkaService mockKafkaService;
+
+
+    @Test
+    public void loadUserByUsername() {
+        User user = new User();
+        user.setLogin("UserLogin");
+        user.setPassword("UserPassword");
+        user.setEmail("user@email.com");
+        user.setRoles(new ArrayList<>());
+        when(mockUserRepository.getByLogin("UserLogin"))
+                .thenReturn(user);
+        UserServiceImpl service = new UserServiceImpl();
+        service.setUserRepository(this.mockUserRepository);
+        org.springframework.security.core.userdetails.User userDetails = new org.springframework.security.core.userdetails.User(user.getLogin(),
+                user.getPassword(), new ArrayList<>());
+        assertEquals(service.loadUserByUsername("UserLogin"), userDetails);
+    }
+
+}
