f0651f9a59105d0dac66151ec14e57785964dc00
lasonoff <lason-vrn@yandex.ru>
2022-11-14 22:53:02 +0300

    Code review

--- kfmailsender/src/main/java/ru/yauroff/kfapplication/kfmailsender/service/impl/KafkaServiceImpl.java
+++ kfmailsender/src/main/java/ru/yauroff/kfapplication/kfmailsender/service/impl/KafkaServiceImpl.java
@@ -1,25 +1,21 @@
 package ru.yauroff.kfapplication.kfmailsender.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import ru.yauroff.kfapplication.kfdto.KfRegistrationUserDto;
 import ru.yauroff.kfapplication.kfmailsender.service.KafkaService;
 import ru.yauroff.kfapplication.kfmailsender.service.MailSenderService;
-import ru.yauroff.kfapplication.kfdto.KfRegistrationUserDto;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaServiceImpl implements KafkaService {
 
-    private MailSenderService mailSenderService;
-
-    @Autowired
-    public void setMailSenderService(MailSenderService mailSenderService) {
-        this.mailSenderService = mailSenderService;
-    }
+    private final MailSenderService mailSenderService;
 
     @Override
-    @KafkaListener(topics="userRegistrationTopic")
+    @KafkaListener(topics = "userRegistrationTopic")
     public void listener(ConsumerRecord<Long, KfRegistrationUserDto> record) {
         mailSenderService.sendMail(record.key(), record.value());
     }
--- kfmailsender/src/main/java/ru/yauroff/kfapplication/kfmailsender/service/impl/MailSenderServiceImpl.java
+++ kfmailsender/src/main/java/ru/yauroff/kfapplication/kfmailsender/service/impl/MailSenderServiceImpl.java
@@ -1,7 +1,7 @@
 package ru.yauroff.kfapplication.kfmailsender.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,25 +9,21 @@ import org.springframework.stereotype.Service;
 import ru.yauroff.kfapplication.kfdto.KfRegistrationUserDto;
 import ru.yauroff.kfapplication.kfmailsender.service.MailSenderService;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class MailSenderServiceImpl implements MailSenderService {
-    @Value("${spring.mail.send}")
-    private boolean sendMail;
-
+    private final JavaMailSender mailSender;
+    @Value("${spring.mail.not.send}")
+    private boolean notSendMail;
     @Value("${spring.mail.username}")
     private String mailSendFrom;
 
-    private JavaMailSender mailSender;
-
-    @Autowired
-    public MailSenderServiceImpl(JavaMailSender emailSender) {
-        this.mailSender = emailSender;
-    }
-
     public void sendMail(Long userId, KfRegistrationUserDto kfRegistrationUserDto) {
-        if (!sendMail) {
-            writeMailToLog(kfRegistrationUserDto);
+        if (notSendMail) {
+            log.info("Not send mail for user with e-mail: " + kfRegistrationUserDto.getEmail());
         } else {
             SimpleMailMessage message = new SimpleMailMessage();
             message.setFrom(mailSendFrom);
@@ -35,16 +31,12 @@ public class MailSenderServiceImpl implements MailSenderService {
             message.setSubject("Registration!");
             message.setText("Hi, " + kfRegistrationUserDto.getFirstname() + "!\n\n"
                     + "You are registered on web service with login: " + kfRegistrationUserDto.getLogin() + ".");
-            mailSender.send(message);
+            //TODO:
+            CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> mailSender.send(message))
+                                                                         .exceptionally(ex -> {
+                                                                             log.error(ex.getMessage());
+                                                                             return null;
+                                                                         });
         }
-    }
-
-    private void writeMailToLog(KfRegistrationUserDto kfRegistrationUserDto) {
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            log.error(e.getMessage());
-        }
-        log.info("Not send mail for user with e-mail: " + kfRegistrationUserDto.getEmail());
     }
 }
